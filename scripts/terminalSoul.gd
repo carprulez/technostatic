941271b2a98aa6214d478f338b14ddcba14b37fd
carprulez <147204410+carprulez@users.noreply.github.com>
2026-05-16 21:08:05 -0700

    wrote cutscene screens

---
+++ scripts/terminalSoul.gd
@@ -0,0 +1,76 @@
+extends Control
+
+@onready var user_input = $LineEdit
+@onready var overlay = $TerminalOverlay
+var newOverlay = load("res://assets/ui/terminalOverlay6.png")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Puzzle.visible = false
+	user_input.editable = false
+	$StaticText.visible = false
+	$FlavorText.visible = false
+	$GravFail.visible = false
+	$AirFilter.visible = false
+	$SystemReboot.visible = false
+	$ReturnButton.visible = false
+	$Puzzle/File4.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	overlay.texture = newOverlay
+	$StaticText.visible = true
+	$FlavorText.visible = true
+	user_input.editable = true
+
+
+func _on_line_edit_text_submitted(new_text: String) -> void:
+	if new_text == "bariss":
+		$StaticText.visible = false
+		$Puzzle.visible = true
+	if new_text == "THANK YOU":
+		$Puzzle/File4.visible = true
+	user_input.clear()
+
+
+func _on_texture_button_pressed() -> void:
+	print("SUCCESS")
+
+
+func _on_file_1_pressed() -> void:
+	$Puzzle.visible = false
+	$GravFail.visible = true
+	$ReturnButton.visible = true
+
+
+func _on_file_2_pressed() -> void:
+	$Puzzle.visible = false
+	$AirFilter.visible = true
+	$ReturnButton.visible = true
+
+
+func _on_file_3_pressed() -> void:
+	$Puzzle.visible = false
+	$SystemReboot.visible = true
+	$ReturnButton.visible = true
+
+
+func _on_return_button_pressed() -> void:
+	$ReturnButton.visible = false
+	$GravFail.visible = false
+	$AirFilter.visible = false
+	$SystemReboot.visible = false
+	$Puzzle.visible = true
+
+
+func _on_file_4_pressed() -> void:
+	$Puzzle.visible = false
+	$StaticText.text = "THANK YOU FOR EMPOWERING US"
+	$StaticText/TextureButton.visible = false
+	$StaticText.visible = true
